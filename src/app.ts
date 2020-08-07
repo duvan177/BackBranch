562bb957715df7b11c81f292b8c9b340f3fca435
duvan narvaez <47201336+duvan177@users.noreply.github.com>
2020-08-07 01:00:17 -0500

    Fix Headers

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,11 @@ export class App {
     middlewares() {
         this.app.use(morgan('dev'));
         this.app.use(express.json());
+        this.app.use((req, res, next) =>{
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next()
+        });
     }
 
     routes() {
